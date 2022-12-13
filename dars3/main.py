064623cfe49a7e15990699c96edf3f5c80882369
Turobov Ozodbek 0525 <118830351+OzodbekTurobov@users.noreply.github.com>
2022-12-13 19:20:09 +0500

    Add files via upload

---
+++ dars3/main.py
@@ -0,0 +1,14 @@
+a = input("parol kiriting: ")
+b = input("login kiriting: ")
+
+while True:
+    #a = input("parol kiriting: ")
+    #b = input("login kiriting: ")
+    if a == "880088":
+        print("to`g`ri")
+
+    elif b == "0808":
+        print("to`g`ri")
+        break
+    else:
+        print("Xato")
